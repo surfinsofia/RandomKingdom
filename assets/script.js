87f82ccdb59240f5fa8df6d147ee641b7cfea114
Sofia <asofiae96@gmail.com>
2022-09-01 20:29:07 -0500

    button welcome

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+console.log('aloha');
+
+function nopath () {
+    alert('haha')
+    }
+    
+
+
+function yespath () {
+document.getElementById("yes").onclick = 
+    function fun() {  
+    document.getElementById("yes").innerHTML = "Are you sure?";  
+    document.getElementsByTagName("body")[0].style.color = "blue";  
+    document.getElementsByTagName("body")[0].style.backgroundColor = "lightgreen";   
+    }  
+};
+
+function yespath () {
+    document.getElementById("yes").onclick = 
+        function fun() {  
+        document.getElementById("yes").innerHTML = "Welcome";  
+        document.getElementsByTagName("body")[0].style.color = "blue";  
+        document.getElementsByTagName("body")[0].style.backgroundColor = "lightgreen";   
+        }  
+    };
